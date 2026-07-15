eb08b77133b32245f99f83348cafa74da73c2c2a
DeadChannelDice <92655490+DeadChannelDice@users.noreply.github.com>
2026-07-15 18:18:47 -0500

    Updated wizrd favicon to dead channel favicon.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,33 +1,33 @@
-import NavBar from '@/components/NavBar'
-import '@/styles/globals.css'
-import { Montserrat } from "next/font/google"
-import Head from 'next/head'
-import Footer from "@/components/Footer.js"
-import { AnimatePresence } from 'framer-motion'
-import { useRouter } from 'next/router'
+import NavBar from "@/components/NavBar";
+import "@/styles/globals.css";
+import { Montserrat } from "next/font/google";
+import Head from "next/head";
+import Footer from "@/components/Footer.js";
+import { AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
-  variable: "--font-mont"
-})
-
-
+  variable: "--font-mont",
+});
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/WizardFavicon.ico" />
+        <link rel="icon" href="/deadChannelFavicon.ico" />
       </Head>
-      <main className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}>
-          <NavBar />
-          <AnimatePresence mode="wait">
+      <main
+        className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}
+      >
+        <NavBar />
+        <AnimatePresence mode="wait">
           <Component key={router.asPath} {...pageProps} />
-          </AnimatePresence>
-          <Footer />
+        </AnimatePresence>
+        <Footer />
       </main>
     </>
-  )
+  );
 }
