326dc9725e1b00f6c66f3eb926b1e9d98dc2abea
Michael Charpin <michael.charpin@gmail.com>
2026-08-06 16:18:07 -0500

    Updated Favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -17,7 +17,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/deadChannelFavicon.ico" />
+        <link rel="icon" href="/MC.ico" />
       </Head>
       <main
         className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}
